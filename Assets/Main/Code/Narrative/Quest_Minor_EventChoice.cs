3e50e1f8947d0dc3d02b0d6f9ce9420ecd94ccc5
MichaelRSheridan <39051851+MichaelRSheridan@users.noreply.github.com>
2019-11-08 16:37:33 -0500

    Add Quest System

---
+++ Assets/Main/Code/Narrative/Quest_Minor_EventChoice.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using PixelCrushers.DialogueSystem;
+
+public class Quest_Minor_EventChoice : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DialogueManager.StartConversation("Quest_Minor_Prefab", gameObject.transform, gameObject.transform, 0);
+
+
+
+            DialogueLua.SetVariable("Quest_Minor_EventTitle", "WHAT HAPPENNED?");
+
+        DialogueLua.SetVariable("Quest_Minor_EventChoice_1", "EARTHQUAKE");
+        DialogueLua.SetVariable("Quest_Minor_EventChoice_2", "BUILDING COLLAPSE");
+        DialogueLua.SetVariable("Quest_Minor_EventChoice_3", "ELDRITCH HORROR");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
