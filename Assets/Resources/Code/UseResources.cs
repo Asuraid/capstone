5156d9c50a644e413fbf4c2de4a317313f71c3ee
Andreea Pop <andreeabiancapop@gmail.com>
2020-01-08 14:58:06 -0500

    Got output function to work; needs work on if the numbers are different between used and gained

--- Assets/Resources/Code/UseResources.cs
+++ Assets/Resources/Code/UseResources.cs
@@ -1,40 +1,93 @@
 ﻿using UnityEngine;
 using TeamMars.Capstone.Manager.Resources;
 
-public class UseResources : MonoBehaviour
+namespace TeamMars.Capstone.Manager.Resources
 {
-    public enum UsedResources // your custom enumeration
+    public class UseResources : MonoBehaviour
     {
-        None,
-        RawGame,
-        RawFish,
-        Wood,
-    };
-    [Header("Used Resources")]
-    [Tooltip("What resource will be used from this area; can be multiple.")]
-    public bool usingRawFish;
-    public bool usingCookedFish;
+        public enum UsedResources // your custom enumeration
+        {
+            None,
+            RawGame,
+            RawFish,
+            Wood,
+        };
+        [Header("Used Resources")]
+        [Tooltip("What resource will be used from this area; can be multiple.")]
+        public bool usingRawFish;
+        public bool usingCookedFish;
 
-    public bool usingRawMeat;
-    public bool usingCookedMeat;
+        public bool usingRawMeat;
+        public bool usingCookedMeat;
 
-    public int usedAmount;
+        public int usedAmount;
 
-    [Header("Resource Output")]
-    [Tooltip("What resource will be gained from this area; can be multiple.")]
-    public bool hasAnOutput;
+        [Header("Resource Output")]
+        [Tooltip("What resource will be gained from this area; can be multiple.")]
+        public bool hasAnOutput;
 
-    public bool gainRawFish;
-    public bool gainCookedFish;
+        public bool gainRawFish;
+        public bool gainCookedFish;
 
-    public bool gainRawMeat;
-    public bool gainCookedMeat;
+        public bool gainRawMeat;
+        public bool gainCookedMeat;
 
-    public int gainedAmount;
+        public int gainedAmount;
 
-    private void OnMouseDown()
-    {
-        print("do thing here");
+        private void OnMouseDown()
+        {
+            // If the item outputs things.
+            if (hasAnOutput)
+            {
+                if (gainRawFish)
+                {
+                    ResourceManager.Instance.AddRawFish(gainedAmount);
+                }
 
+                if (gainCookedFish && GameManager.Instance.rawFish > 0)
+                {
+                    ResourceManager.Instance.AddCookedFish(gainedAmount);
+                }
+                else
+                {
+                    print(GameManager.Instance.rawFish);
+                    print("Not enough materials.");
+                }
+
+                if (gainRawMeat)
+                {
+                    ResourceManager.Instance.AddRawGame(gainedAmount);
+                }
+
+                if (gainCookedMeat && GameManager.Instance.rawGame > 0)
+                {
+                    ResourceManager.Instance.AddCookedGame(gainedAmount);
+                }
+                else
+                {
+                    print("Not enough materials.");
+                }
+            }
+
+            if (usingRawFish && GameManager.Instance.rawFish > 0)
+            {
+                ResourceManager.Instance.UseRawFish(usedAmount);
+            }
+
+            if (usingCookedFish)
+            {
+                ResourceManager.Instance.UseCookedFish(usedAmount);
+            }
+
+            if (usingRawMeat)
+            {
+                ResourceManager.Instance.UseRawGame(usedAmount);
+            }
+
+            if (usingCookedMeat)
+            {
+                ResourceManager.Instance.UseCookedGame(usedAmount);
+            }
+        }
     }
 }
