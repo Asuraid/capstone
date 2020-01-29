bd32a7eabd832d4e75739b3b33df3dd4fa164dfc
Andreea Pop <andreeabiancapop@gmail.com>
2020-01-29 13:29:19 -0500

    Add feedback for gaining materials

--- Assets/Capstone/Code/Managers/ResourceManager.cs
+++ Assets/Capstone/Code/Managers/ResourceManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using TMPro;
+using System.Collections;
 
 namespace TeamMars.Capstone.Manager.Resources
 {
@@ -41,78 +42,117 @@ namespace TeamMars.Capstone.Manager.Resources
         public void AddRawGame(int amount)
         {
             GameManager.Instance.rawGame += amount;
+            rawGame.GetComponent<Text_Bump>().Bump();
             UpdateResources();
         }
 
         public void AddRawFish(int amount)
         {
             GameManager.Instance.rawFish += amount;
+            rawFish.GetComponent<Text_Bump>().Bump();
             UpdateResources();
         }
 
         public void AddCookedGame(int amount)
         {
             GameManager.Instance.cookedGame += amount;
+            cookedGame.GetComponent<Text_Bump>().Bump();
             UpdateResources();
         }
 
         public void AddCookedFish(int amount)
         {
             GameManager.Instance.cookedFish += amount;
+            cookedFish.GetComponent<Text_Bump>().Bump();
             UpdateResources();
         }
 
         public void AddRawWood(int amount)
         {
             GameManager.Instance.rawWood += amount;
+            rawWood.GetComponent<Text_Bump>().Bump();
             UpdateResources();
         }
 
         public void AddRefinedWood(int amount)
         {
             GameManager.Instance.refinedWood += amount;
+            refinedWood.GetComponent<Text_Bump>().Bump();
             UpdateResources();
         }
         #endregion
 
         #region Using resources
+        /// <summary>
+        /// Use a set amount of raw game.
+        /// </summary>
+        /// <param name="amount">Amount of raw game.</param>
         public void UseRawGame(int amount)
         {
             GameManager.Instance.rawGame -= amount;
+            rawGame.GetComponent<Text_Bump>().Bump();
             UpdateResources();
         }
 
+        /// <summary>
+        /// Use a set amount of raw fish.
+        /// </summary>
+        /// <param name="amount">Amount of raw fish.</param>
         public void UseRawFish(int amount)
         {
             GameManager.Instance.rawFish -= amount;
+            rawFish.GetComponent<Text_Bump>().Bump();
             UpdateResources();
         }
 
+        /// <summary>
+        /// Use a set amount of cooked game.
+        /// </summary>
+        /// <param name="amount">Amount of cooked game.</param>
         public void UseCookedGame(int amount)
         {
             GameManager.Instance.cookedGame -= amount;
+            cookedGame.GetComponent<Text_Bump>().Bump();
             UpdateResources();
         }
 
+        /// <summary>
+        /// Use a set amount of cooked fish.
+        /// </summary>
+        /// <param name="amount">Amount of cooked fish.</param>
         public void UseCookedFish(int amount)
         {
             GameManager.Instance.cookedFish -= amount;
+            cookedFish.GetComponent<Text_Bump>().Bump();
             UpdateResources();
         }
 
+        /// <summary>
+        /// Use a set amount of raw wood.
+        /// </summary>
+        /// <param name="amount">Amount of raw wood.</param>
         public void UseRawWood(int amount)
         {
             GameManager.Instance.rawWood -= amount;
+            rawWood.GetComponent<Text_Bump>().Bump();
             UpdateResources();
         }
 
+        /// <summary>
+        /// Use a set amount of refined wood.
+        /// </summary>
+        /// <param name="amount">Amount of refined wood.</param>
         public void UseRefinedWood(int amount)
         {
             GameManager.Instance.refinedWood -= amount;
+            refinedWood.GetComponent<Text_Bump>().Bump();
             UpdateResources();
         }
         #endregion
 
+        /// <summary>
+        /// Update all resource texts using the variables located in the Game Manager.
+        /// </summary>
         public void UpdateResources()
         {
             rawFish.text = GameManager.Instance.rawFish.ToString();
---
+++ Assets/Capstone/Code/UI/Text_Bump.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TeamMars.Capstone
+{
+
+    public class Text_Bump : MonoBehaviour
+    {
+        [Header("Bump")]
+        /// whether or not the bar should "bump" when changing value
+        public bool BumpScaleOnChange = true;
+        /// the duration of the bump animation
+        public float BumpDuration = 0.2f;
+        /// the curve to map the bump animation on
+        public AnimationCurve BumpAnimationCurve = AnimationCurve.Linear(0f, 1f, 1f, 1f);
+        /// whether or not the bar is bumping right now
+        public bool Bumping { get; protected set; }
+
+        protected float _targetFill;
+        protected Vector3 _targetLocalScale = Vector3.one;
+        protected float _newPercent;
+        protected float _lastUpdateTimestamp;
+        protected bool _bump = false;
+        protected Vector3 _initialScale;
+        protected Vector3 _newScale;
+
+        protected Image _foregroundImage;
+        protected Image _delayedImage;
+        protected bool _initialized;
+
+        public static Text_Bump Instance;
+
+        /// <summary>
+        /// On start we store our image component
+        /// </summary>
+        protected virtual void Start()
+        {
+            _initialScale = transform.localScale;
+
+            _initialized = true;
+
+            if (Instance == null) Instance = this;
+        }
+
+        /// <summary>
+        /// Triggers a camera bump
+        /// </summary>
+        public virtual void Bump()
+        {
+            if (!BumpScaleOnChange || !_initialized)
+            {
+                return;
+            }
+            if (gameObject.activeInHierarchy)
+            {
+                StartCoroutine(BumpCoroutine());
+            }
+        }
+
+        /// <summary>
+        /// A coroutine that (usually quickly) changes the scale of the bar 
+        /// </summary>
+        /// <returns>The coroutine.</returns>
+        protected virtual IEnumerator BumpCoroutine()
+        {
+            float journey = 0f;
+            Bumping = true;
+
+            while (journey <= BumpDuration)
+            {
+                journey = journey + Time.deltaTime;
+                float percent = Mathf.Clamp01(journey / BumpDuration);
+                float curvePercent = BumpAnimationCurve.Evaluate(percent);
+                transform.localScale = curvePercent * _initialScale;
+
+                yield return null;
+            }
+            Bumping = false;
+            yield return null;
+        }
+    }
+}
