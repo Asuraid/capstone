734cbf843a1dfdfedc86773c8df8ad15ddb6a86c
MichaelRSheridan <39051851+MichaelRSheridan@users.noreply.github.com>
2020-03-04 13:27:59 -0500

    Commit _ Week07

---
+++ Assets/Capstone/Code/UI/TimeChange_UI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace TeamMars.Capstone.Manager
+{
+    public class TimeChange_UI : MonoBehaviour
+    {
+        public Sprite[] TimeImages = new Sprite[5];
+
+        public SpriteRenderer TimeUI;
+        // Start is called before the first frame update
+        void Start()
+        {
+            TimeImages = new Sprite[5];
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            TimeUI.sprite = TimeImages[GameManager.Instance.hours];
+        }
+    }
+}
