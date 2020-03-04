109d5bb34167091fbd68b6782aa1157cc682fba6
MichaelRSheridan <39051851+MichaelRSheridan@users.noreply.github.com>
2020-03-04 15:12:41 -0500

    Introduction Sequence

---
+++ Assets/Capstone/Code/Narrative/IntroductionSequence.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+public class IntroductionSequence : MonoBehaviour
+{
+
+    public DialogueDatabase diaData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        diaData = GetComponent<DialogueSystemController>().initialDatabase;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(DialogueLua.GetVariable("WhatCharacter").AsInt);   
+    }
+}
