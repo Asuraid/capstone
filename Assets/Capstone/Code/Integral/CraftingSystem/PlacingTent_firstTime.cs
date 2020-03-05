2a251b174bdce754ce365c55781f402aac3e3ad8
MichaelRSheridan <39051851+MichaelRSheridan@users.noreply.github.com>
2020-03-05 12:27:12 -0500

    TentCode

---
+++ Assets/Capstone/Code/Integral/CraftingSystem/PlacingTent_firstTime.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacingTent_firstTime : MonoBehaviour
+{
+
+    public GameObject TentGameobject;
+    public GameObject UI_Background;
+    bool isDone = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDone = false;
+        TentGameobject.SetActive(false);
+        UI_Background.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        if (isDone == false)
+        {
+            TentGameobject.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+            TentGameobject.transform.position = new Vector3(TentGameobject.transform.position.x, TentGameobject.transform.position.y,transform.position.z);
+            TentGameobject.SetActive(true);
+            UI_Background.SetActive(false);
+            isDone = true;
+        }
+    }
+}
